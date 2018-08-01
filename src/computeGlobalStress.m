a700088d049365e05a6fd7ffd7a6d4382f42f97d
Pablo Vicente Munuera <pvicente1@us.es>
2018-08-01 16:26:59 +0200

    code nicely formatted

--- src/computeGlobalStress.m
+++ src/computeGlobalStress.m
@@ -5,65 +5,67 @@ function [] = computeGlobalStress(areaOfCells, cellInfo, edgeInfo, validCells)
 %   "The mechanical anisotropy in a tissue promotes ordering in hexagonal 
 %   cell packing", Kaoru Sugimura and Shuji Ishihara
 
-%% Parameters notation
-% N_uv      -> Global stress tensor
-% Delta_uv  -> Kronecker's delta
-% pressure (P) -> Pressure of the cell
-% tension (T)         -> Tension of the edge
-% area (A)  -> Area of the cell
-% edgeLength (l)         -> Edge length
-% lx        -> Component X of edge length l
-% ly        -> Component Y of edge length l
+    %% Parameters notation
+    % N_uv      -> Global stress tensor
+    % Delta_uv  -> Kronecker's delta
+    % pressure (P) -> Pressure of the cell
+    % tension (T)         -> Tension of the edge
+    % area (A)  -> Area of the cell
+    % edgeLength (l)         -> Edge length
+    % lx        -> Component X of edge length l
+    % ly        -> Component Y of edge length l
 
 
-local_N_uv = cell(2, 2);
-global_N_uv = zeros(2, 2);
-% if u is 1, represents 'x' coordinate, otherwise -> 'y'
-for u = 1:2
-    % if v is 1, represents 'x' coordinate, otherwise -> 'y'
-    for v = 1:2
-        localStress = zeros(size(cellInfo, 1), 1);
-        %Kronecker's delta
-        %The function is 1 if the variables are equal, and 0 otherwise
-        %So, if u = v, 1; u ~= v, 0.
-        if u == v
-            Delta_uv = 1;
-        else
-            Delta_uv = 0;
-        end
-        for numCell = 1:size(cellInfo, 1)
-            actualEdgeInfo = edgeInfo(edgeInfo.ConnectingCell1 == numCell | edgeInfo.ConnectingCell2 == numCell, :);
-
-            if isempty(actualEdgeInfo) == 0 && validCells(numCell)
-                edgesStress = zeros(size(actualEdgeInfo, 1), 1);
-                for numEdge = 1:size(actualEdgeInfo, 1)
-                    %Checking if we have to inverse the vector
-                    if actualEdgeInfo.ConnectingCell1(numEdge) ~= numCell
-                        inverse = 1;
-                    else
-                        inverse = 0;
-                    end
-                    
-                    edgesStress(numEdge) = calculateEdgeStress(actualEdgeInfo, numEdge, inverse, u, v);
-                end
-                cellStress(numCell) = -(areaOfCells(numCell) * cellInfo(numCell, 2) * Delta_uv );
-                %Global stress
-                localStress(numCell) =  (cellStress(numCell) + sum(edgesStress)) / areaOfCells(numCell);
+    numDimensions = 1;
+    
+    local_N_uv = cell(2, 2);
+    global_N_uv = zeros(2, 2);
+    % if u is 1, represents 'x' coordinate, otherwise -> 'y'
+    for u = 1:2
+        % if v is 1, represents 'x' coordinate, otherwise -> 'y'
+        for v = 1:2
+            localStress = zeros(size(cellInfo, 1), numDimensions);
+            %Kronecker's delta
+            %The function is 1 if the variables are equal, and 0 otherwise
+            %So, if u = v, 1; u ~= v, 0.
+            if u == v
+                Delta_uv = 1;
+            else
+                Delta_uv = 0;
             end
+            for numCell = 1:size(cellInfo, 1)
+                actualEdgeInfo = edgeInfo(edgeInfo.ConnectingCell1 == numCell | edgeInfo.ConnectingCell2 == numCell, :);
+
+                if isempty(actualEdgeInfo) == 0 && validCells(numCell)
+                    edgesStress = zeros(size(actualEdgeInfo, 1), numDimensions);
+                    for numEdge = 1:size(actualEdgeInfo, 1)
+                        %Checking if we have to inverse the vector
+                        if actualEdgeInfo.ConnectingCell1(numEdge) ~= numCell
+                            inverse = 1;
+                        else
+                            inverse = 0;
+                        end
+
+                        edgesStress(numEdge, :) = calculateEdgeStress(actualEdgeInfo, numEdge, inverse, u, v);
+                    end
+                    cellStress(numCell) = -(areaOfCells(numCell) * cellInfo(numCell, 2) * Delta_uv );
+                    %Global stress
+                    localStress(numCell, :) =  (cellStress(numCell) + sum(edgesStress)) / areaOfCells(numCell);
+                end
+            end
+
+            for numEdge = 1:size(edgeInfo, 1)
+                globalEdgeStress(numEdge, :) = calculateEdgeStress(edgeInfo, numEdge, 0, u, v);
+            end
+
+    %         validCells = logical(zeros(size(areaOfCells)));
+    %         validCells(cellStress~=0) = 1;
+            globalStress = (sum(cellStress) + sum(globalEdgeStress))/sum([areaOfCells]);
+
+            local_N_uv(u, v) = {localStress(validCells)};
+            global_N_uv(u, v) = globalStress;
         end
-        
-        for numEdge = 1:size(edgeInfo, 1)
-            globalEdgeStress(numEdge) = calculateEdgeStress(edgeInfo, numEdge, 0, u, v);
-        end
-        
-%         validCells = logical(zeros(size(areaOfCells)));
-%         validCells(cellStress~=0) = 1;
-        globalStress = (sum(cellStress) + sum(globalEdgeStress))/sum([areaOfCells]);
-        
-        local_N_uv(u, v) = {localStress(validCells)};
-        global_N_uv(u, v) = globalStress;
     end
-end
 
 
 %Calculate the eigenvectors of the global stress tensor
